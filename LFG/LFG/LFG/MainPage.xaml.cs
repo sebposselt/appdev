2b4f92138a5a990430d1bdf256e3f4ba92cacfdb
Douglas Lewis <douglaslewis56@outlook.com>
2018-09-16 15:51:08 +1000

    Project creation & basic navigation skeleton

---
+++ LFG/LFG/LFG/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LFG
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void Profile_Button_Clicked(object sender, EventArgs e) {
+            await Navigation.PushAsync(new Profile());
+        }
+
+        private async void Matchmaking_Button_Clicked(object sender, EventArgs e) {
+            await Navigation.PushAsync(new Matchmaking());
+        }
+    }
+}
---
+++ LFG/LFG/LFG/Profile.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LFG
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Profile : ContentPage
+	{
+		public Profile ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Mainmenu_Clicked_Button(object sender, EventArgs e) {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void Matchmaking_Clicked_Button(object sender, EventArgs e) {
+            await Navigation.PushAsync(new Matchmaking());
+        }
+    }
+}
