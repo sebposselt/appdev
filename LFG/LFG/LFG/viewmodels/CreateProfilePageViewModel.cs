168998d511a6bcaaf229f48113eb8a04ff96e99a
Douglas Lewis <douglaslewis56@outlook.com>
2018-10-06 17:17:14 +1000

    Profile Storage onSave

--- LFG/LFG/LFG/viewmodels/CreateProfilePageViewModel.cs
+++ LFG/LFG/LFG/viewmodels/CreateProfilePageViewModel.cs
@@ -2,26 +2,42 @@
 using System.Windows.Input;
 using LFG.models;
 using Xamarin.Forms;
+using PCLStorage;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
 
 namespace LFG.viewmodels
 {
-    public class CreateProfilePageViewModel : ViewModelBase
-    {
+    [DataContract]
+    public class CreateProfilePageViewModel : ViewModelBase {
 
+        [DataMember]
         public Profile PlayerProfile;
         public ICommand SaveCommand { get; private set; }
 
-        public CreateProfilePageViewModel()
-        {
+        public CreateProfilePageViewModel() {
             PlayerProfile = new Profile();
-            SaveCommand = new Command(Save);
+            SaveCommand = new Command(async () => await SaveAsync());
         }
 
 
         //TODO function to save PlayerProfile
-        public void Save()
-        {
+        public async Task SaveAsync() {
+            await PCLSPlayerProfile(PlayerProfile);
             Console.WriteLine("Saved!");
+        }
+
+        public async Task PCLSPlayerProfile<T>(T PlayerProfile) {
+            var ms = new MemoryStream();
+            var js = new DataContractJsonSerializer(typeof(T));
+            js.WriteObject(ms, PlayerProfile);
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("MyProfile", CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync("MyPlayerProfile" + ".json", CreationCollisionOption.ReplaceExisting);
+            StreamReader sr = new StreamReader(ms);
+            await file.WriteAllTextAsync(sr.ReadToEnd());
         }
     }
 }
