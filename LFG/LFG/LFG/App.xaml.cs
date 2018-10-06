61dc13c20e2332f97e803b8690736ad9fa5b65aa
Sebastian P <npm981@ku.dk>
2018-10-06 14:39:55 +1000

    function to add dummy profile.. used for development

--- LFG/LFG/LFG/App.xaml.cs
+++ LFG/LFG/LFG/App.xaml.cs
@@ -2,16 +2,22 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using LFG.views;
+using LFG.models;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace LFG
 {
     public partial class App : Application
     {
+
+
+        private Profile _user;
+
         public App()
         {
             InitializeComponent();
-
+            _user = new Profile();
+            dummyprofile();
 			MainPage = new NavigationPage(new WelcomePage());
         }
 
@@ -28,6 +34,42 @@ namespace LFG
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        /// <summary>
+        /// Dummyprofile is a function for development purposes.
+        /// </summary>
+        private void dummyprofile()
+        {
+            _user.Username = "Fry";
+            _user.Region = "Europe";
+            _user.Language = "English";
+            _user.Age = "46-55";
+            _user.ProfileText = "My name is Phillip J. Fry, and I come from the year 2000";
+            _user.SteamTag = "FrenchFry";
+            _user.DiscordTag = "FrenchFry#5556";
+            _user.XboxLiveTag = "FrenchFry";
+            _user.PSNTag = "FrenchFry";
+
+            _user.Game1.Title = "CSGO1";
+            _user.Game1.Platform = "PC";
+            _user.Game1.SkillLevel = "Experinced";
+
+            _user.Game2.Title = "CSGO2";
+            _user.Game2.Platform = "PC";
+            _user.Game2.SkillLevel = "Experinced";
+
+            _user.Game3.Title = "CSGO3";
+            _user.Game3.Platform = "PC";
+            _user.Game3.SkillLevel = "Experinced";
+
+            _user.Game4.Title = "CSGO4";
+            _user.Game4.Platform = "PC";
+            _user.Game4.SkillLevel = "Experinced";
+
+            _user.Game5.Title = "CSGO5";
+            _user.Game5.Platform = "PC";
+            _user.Game5.SkillLevel = "Experinced";
         }
     }
 }
