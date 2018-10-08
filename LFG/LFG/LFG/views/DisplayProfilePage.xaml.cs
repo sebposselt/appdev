49afbf19276249ebb9a5858c68378ebee6f863aa
Sebastian P <npm981@ku.dk>
2018-10-08 12:12:17 +1000

    found way to pass object from app class to specific page constructor

--- LFG/LFG/LFG/views/DisplayProfilePage.xaml.cs
+++ LFG/LFG/LFG/views/DisplayProfilePage.xaml.cs
@@ -9,16 +9,13 @@ namespace LFG.views
     public partial class DisplayProfilePage : ContentPage
     {
         private DisplayProfilePageViewModel _displayProfilePageViewModel;
-        private Profile _user;
 
         public DisplayProfilePage()
         {
 
 
-            _user = new Profile();
-            dummyprofile();
-            _displayProfilePageViewModel = new DisplayProfilePageViewModel(_user);
-            //BindingContext = _displayProfilePageViewModel;
+            var app = Application.Current as App;
+            _displayProfilePageViewModel = new DisplayProfilePageViewModel(app.User);
             InitializeComponent();
             //NavigationPage.HasNavigationBar = "false"
 
@@ -26,37 +23,6 @@ namespace LFG.views
 
 
 
-        private void dummyprofile()
-        {
-            _user.Username = "Fry";
-            _user.Region = "Europe";
-            _user.Language = "English";
-            _user.Age = "46-55";
-            _user.ProfileText = "My name is Phillip J. Fry, and I come from the year 2000";
-            _user.SteamTag = "FrenchFry";
-            _user.DiscordTag = "FrenchFry#5556";
-            _user.XboxLiveTag = "FrenchFry";
-            _user.PSNTag = "FrenchFry";
 
-            _user.Game1.Title = "CSGO1";
-            _user.Game1.Platform = "PC";
-            _user.Game1.SkillLevel = "Experinced";
-
-            _user.Game2.Title = "CSGO2";
-            _user.Game2.Platform = "PC";
-            _user.Game2.SkillLevel = "Experinced";
-
-            _user.Game3.Title = "CSGO3";
-            _user.Game3.Platform = "PC";
-            _user.Game3.SkillLevel = "Experinced";
-
-            _user.Game4.Title = "CSGO4";
-            _user.Game4.Platform = "PC";
-            _user.Game4.SkillLevel = "Experinced";
-
-            _user.Game5.Title = "CSGO5";
-            _user.Game5.Platform = "PC";
-            _user.Game5.SkillLevel = "Experinced";
-        }
     }
 }
