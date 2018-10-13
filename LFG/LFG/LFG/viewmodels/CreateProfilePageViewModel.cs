4c058d7e1d16690a0669786a21d666a51f897a5f
Douglas Lewis <douglaslewis56@outlook.com>
2018-10-13 13:04:15 +1000

    Added a Deserilization function & Android+IOS removed from UITest

--- LFG/LFG/LFG/viewmodels/CreateProfilePageViewModel.cs
+++ LFG/LFG/LFG/viewmodels/CreateProfilePageViewModel.cs
@@ -7,14 +7,17 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace LFG.viewmodels
 {
     [DataContract]
     public class CreateProfilePageViewModel : ViewModelBase {
 
+
         [DataMember]
         public Profile PlayerProfile;
+        public Profile DeserializedPlayerProfile;
         public ICommand SaveCommand { get; private set; }
 
         public CreateProfilePageViewModel() {
@@ -29,15 +32,31 @@ namespace LFG.viewmodels
             Console.WriteLine("Saved!");
         }
 
-        public async Task PCLSPlayerProfile<T>(T PlayerProfile) {
-            var ms = new MemoryStream();
-            var js = new DataContractJsonSerializer(typeof(T));
-            js.WriteObject(ms, PlayerProfile);
+        public async Task PCLSPlayerProfile<Profile>(Profile playerProfile) {
+            MemoryStream ms = new MemoryStream();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Profile));
+            js.WriteObject(ms, playerProfile);
+            byte[] json = ms.ToArray();
+            ms.Close();
+
             IFolder rootFolder = FileSystem.Current.LocalStorage;
             IFolder folder = await rootFolder.CreateFolderAsync("MyProfile", CreationCollisionOption.OpenIfExists);
             IFile file = await folder.CreateFileAsync("MyPlayerProfile" + ".json", CreationCollisionOption.ReplaceExisting);
+
             StreamReader sr = new StreamReader(ms);
             await file.WriteAllTextAsync(sr.ReadToEnd());
+        }
+
+        public async Task PCLDSPlayerProfile<Profile>(Profile outputPlayerProfile) where Profile : class {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder folder = await rootFolder.CreateFolderAsync("MyProfile", CreationCollisionOption.OpenIfExists);
+            IFile file = await folder.CreateFileAsync("MyPlayerProfile" + ".json", CreationCollisionOption.OpenIfExists);
+
+            string jsonString = await file.ReadAllTextAsync();
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Profile));
+            outputPlayerProfile = js.ReadObject(ms) as Profile;
+            ms.Close();
         }
     }
 }
