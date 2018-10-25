b83736e0023a496aaff99cbe05119fe814c0353b
Minh Bui <n9446443@qut.edu.au>
2018-10-25 12:29:26 +1000

    added close DB
    closed DB when Pull

--- LFG/LFG/LFG/viewmodels/MatchMakingPageViewModel.cs
+++ LFG/LFG/LFG/viewmodels/MatchMakingPageViewModel.cs
@@ -15,7 +15,10 @@ namespace LFG.viewmodels
 
         public void PullFromDB()
         {
-            SqlCommand Pull = new SqlCommand("SELECT TOP 2 Username, Region, Language,Age, DiscordTag FROM LFGdb ORDER by newid()");
+            DB.Open();
+            SqlCommand Pull = new SqlCommand("SELECT TOP 2 Username, Region, Language,Age, DiscordTag FROM LFGdb ORDER by newid()", DB);
+            // show function
+            DB.Close();
 
         }  
     }
